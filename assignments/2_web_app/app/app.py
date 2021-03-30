27846c29507a734980e0f11c8aceb465cb8f0152
austin <aguspdc@gmail.com>
2021-03-29 23:03:03 -0700

    Enabled creation and deletion of tasks

--- assignments/2_web_app/app/app.py
+++ assignments/2_web_app/app/app.py
@@ -4,21 +4,69 @@
 # author: Austin Perzben
 # created: 2021-03-24 | 21:45:27
 
-from flask import Flask, render_template
-import requests
+from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
 
 kanban = Flask(__name__)
 
+kanban.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///kanban.db"
 
-@kanban.route('/')
+db = SQLAlchemy(kanban)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String())
+    content = db.Column(db.String(250))
+    panel = db.Column(db.String())
+
+
+@kanban.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    todos = Task.query.filter_by(panel='todo')
+    doings = Task.query.filter_by(panel='doing')
+    dones = Task.query.filter_by(panel='done')
+    return render_template("index.html", todos=todos, doings=doings, dones=dones, task=None)
 
 
-@kanban.route('/example', methods=['GET', 'POST'])
-def example():
-    return render_template("example.html")
+@kanban.route("/new_task", methods=["GET", "POST"])
+def new_task():
+    if request.method == "POST":
+        task = Task(
+            title=request.form['title'], content=request.form['description'], panel=request.form['panel'])
+        db.session.add(task)
+        db.session.commit()
+
+    return redirect(url_for('index'))
+
+
+@kanban.route("/del_task", methods=["POST"])
+def del_task():
+    if request.method == "POST":
+        task_id = request.form['task_id']
+        # print(task_id)
+        task = Task.query.filter_by(id=task_id)
+        task.delete()
+        db.session.commit()
+    return redirect(url_for('index'))
+
+
+@kanban.route("/test", methods=["POST"])
+def test():
+    return render_template("test.html")
+
+# @kanban.route("/edit_task/<task_id>", methods=["GET", "POST"])
+# def edit_task(task_id):
+#     task = Task.query.filter_by(id=task_id).first_or_404()
+
+#     return redirect(url_for('index'),
+#                     task=task)
+
+
+@kanban.route("/notImplemented/<prev>", methods=["GET", "POST"])
+def notImplemented(prev):
+    return f"<h1>Not Implemented!</h1><p>{prev}</p>"
 
 
 if __name__ == "__main__":
-    kanban.run()
+    kanban.run(debug=True)
---
+++ assignments/2_web_app/app/templates/test.js
@@ -0,0 +1,17 @@
+"use strict";
+$('#myModal').on;
+'show.bs.modal', (e) -  >
+    data;
+status: 200, msg;
+'Good This is';
+if (!data)
+    return e.preventDefault();
+#;
+stops;
+modal;
+from;
+being;
+shown;
+$('#myModalLabel').html("This is your status: <span class='color'>" + data.status + "</span>");
+$('.modal-body').text(data.msg);
+return;

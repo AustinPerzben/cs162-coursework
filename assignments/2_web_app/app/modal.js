c7a52ece2d0c83ca51ce17eea816acca41d7505a
austin <aguspdc@gmail.com>
2021-03-31 00:15:03 -0700

    Remove test files

--- assignments/2_web_app/app/modal.js
+++
@@ -1,8 +0,0 @@
-$('#makeTask').on('show.bs.modal', function (e) {
-    var title = $(e.relatedTarget).data('title');
-    var content = $(e.relatedTarget).data('content');
-    // var panel = $(e.relatedTarget).data('panel');
-    $(e.currentTarget).find('input[name="taskTitle"]').val(title);
-    $(e.currentTarget).find('input[name="taskContent"]').val(content);
-    // $(e.currentTarget).find('input[name="panel"]').val(panel);
-});
